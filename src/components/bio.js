96e95e57b2649d15204e7b2aa8da62890cdb1818
Michał Perlak <michalperlak@windowslive.com>
2019-11-27 23:23:43 +0100

    Small config changes

--- src/components/bio.js
+++ src/components/bio.js
@@ -57,7 +57,7 @@ const Bio = () => {
         Written by <strong>{author}</strong> who lives and works in Kraków building useful things.
         {` `}
         <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
+          Follow him on Twitter
         </a>
       </p>
     </div>
